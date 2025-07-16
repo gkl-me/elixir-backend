6f2946f6df5ab5a6edc23719d85ece312b5320c7
Gokul <gklgrme@gmail.com>
2025-07-16 12:49:44 +0530

    query helper added

---
+++ src/helper/queryParamUtils.ts
@@ -0,0 +1,26 @@
+
+import {ParsedQs} from 'qs'
+
+export function extractStringQueryParams<T extends string>(
+    query: Partial<Record<string,string | ParsedQs | (string | ParsedQs)[]>>,
+    keys:T[]  
+):Partial<Record<T,string>>|undefined{
+    const result:Partial<Record<T,string>> = {}
+
+    let hasValidParam = false;
+
+    for(const key of keys){
+        const value = query[key]
+
+        if(typeof value=='string'){
+            result[key] = value
+            hasValidParam = true
+        }else if(Array.isArray(value) && typeof value[0]=='string'){
+            result[key] = value[0]
+            hasValidParam = true
+        }
+    }
+
+    return hasValidParam?result : undefined
+
+}
