0f9be3dcfa84498a8b8a4ce5f4597f5520577128
Gokul <gklgrme@gmail.com>
2025-07-19 12:54:39 +0530

    refactor middlwares

--- src/middlewares/adminAuthMiddleware.ts
+++ src/middlewares/adminAuthMiddleware.ts
@@ -4,6 +4,7 @@ import { ITokenManager } from "../utils/interfaces/ITokenManager";
 import { Token } from "../di/token";
 import { CustomError } from "../errors/CustomError";
 import { STATUS_CODES } from "../constants/statusCodes";
+import { CONSTANT_MESSAGES } from "../constants/messages";
 
 
 
@@ -13,17 +14,17 @@ export const adminAuth = async (req:Request,res:Response,next:NextFunction) => {
         const tokenManager = container.resolve<ITokenManager>(Token.TokenManager)
         const {token } = req.cookies
         if(!token){
-            throw new CustomError('Unauthorized access login again',STATUS_CODES.UNAUTHORIZED)
+            throw new CustomError(CONSTANT_MESSAGES.UNAUTHORIZED,STATUS_CODES.UNAUTHORIZED)
         }
 
         const validToken = tokenManager.verifyToken(token,'access')
 
         if(!validToken){
-            throw new CustomError('Unauthorized access login again',STATUS_CODES.UNAUTHORIZED)
+            throw new CustomError(CONSTANT_MESSAGES.UNAUTHORIZED,STATUS_CODES.UNAUTHORIZED)
         }
         req.admin = { id:validToken.id,role:validToken.role }
         next()
     } catch (error) {
-        next(new CustomError("Unathorized access login again",STATUS_CODES.UNAUTHORIZED))
+        next(new CustomError(CONSTANT_MESSAGES.UNAUTHORIZED,STATUS_CODES.UNAUTHORIZED))
     }
 }
--- src/middlewares/notFound.ts
+++ src/middlewares/notFound.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from "express";
 import { STATUS_CODES } from "../constants/statusCodes";
-import { errorHandler } from "./errorHandler";
 import { errorResponse } from "../helper/responseHanlder";
-import { ERROR_MESSAGES } from "../constants/errorMessages";
+import { CONSTANT_MESSAGES, } from "../constants/messages";
 
 export const notFound = (req:Request,res:Response) => {
-    errorResponse(res,ERROR_MESSAGES.NOT_FOUND,STATUS_CODES.NOT_FOUND)
+    errorResponse(res,CONSTANT_MESSAGES.NOT_FOUND,STATUS_CODES.NOT_FOUND)
 }
