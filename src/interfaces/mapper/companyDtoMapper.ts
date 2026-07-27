0dac24564617d2b92434ad4f4b764f47e08f39a4
Gokul <gklgrme@gmail.com>
2026-07-27 14:35:03 +0530

    chore

--- src/interfaces/mapper/companyDtoMapper.ts
+++ src/interfaces/mapper/companyDtoMapper.ts
@@ -11,11 +11,7 @@ export class companyDtoMapper {
       email: company.email,
       phone: company.phone,
       website: company?.website,
-<<<<<<< HEAD
       status: company.status,
-=======
-      isBlocked: company.isBlocked,
->>>>>>> origin/week2
       createdAt: company.createdAt,
     };
   }
